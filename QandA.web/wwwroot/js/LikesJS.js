9333d86082074cd4322e3b06d34a9bac31aff93d
Rochel Schulgasser <rochelschulgasser@gmail.com>
2022-04-24 18:30:15 -0400

    edit Java script

--- QandA.web/wwwroot/js/LikesJS.js
+++ QandA.web/wwwroot/js/LikesJS.js
@@ -4,12 +4,11 @@
     setInterval(UpdateLikes(questionId), 1000);
 
 
-    console.log(questionId);
+
+
     $("#add-like").on('click', function () {
 
-    //  $("#add-like").remove();
-      
-        console.log("in");
+
         $('#add-like').addClass('text-danger');
         $('#add-like').prop('disabled', true);
 
@@ -21,19 +20,15 @@
 
         $.post('/home/addlike', { questionId, liked }, function () {
 
-            //$("#add-like").addClass('text-danger');
-            //$("#add-like").prop('disabled', true);
           UpdateLikes(questionId);
-         //   $("#add-like").attr('class', "oi oi-heart text-danger");
-         
-       //  $("#question-id").append(` <br /> <br /> <h1> <span style="font-size: 40px; cursor: pointer;" class="oi oi-heart text-danger">heart</span> </h1>`);
-          
-       
-
+        
            
         });
       
-     
+
+
+
+
 
     });
     function UpdateLikes(id) {
